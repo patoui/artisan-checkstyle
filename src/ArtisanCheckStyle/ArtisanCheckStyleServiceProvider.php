688f4aa02b302334142baf00fa998f47d87a2262
Patrique Ouimet <patrique.ouimet@gmail.com>
2016-09-17 11:08:35 -0400

    Updating config path

--- src/ArtisanCheckStyle/ArtisanCheckStyleServiceProvider.php
+++ src/ArtisanCheckStyle/ArtisanCheckStyleServiceProvider.php
@@ -14,7 +14,7 @@ class ArtisanCheckstyleServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__.'/../config/artisan-phpcs.php' => config_path('artisan-phpcs.php'),
+            __DIR__.'/../../config/artisan-phpcs.php' => config_path('artisan-phpcs.php'),
         ]);
     }
 
