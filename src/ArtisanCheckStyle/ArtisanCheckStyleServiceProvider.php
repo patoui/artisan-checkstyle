8337be439777be682327242eeba0d86e42fb424a
Patrique Ouimet <patrique.ouimet@gmail.com>
2017-05-24 21:01:28 -0400

    Updating config publish value

--- src/ArtisanCheckStyle/ArtisanCheckStyleServiceProvider.php
+++ src/ArtisanCheckStyle/ArtisanCheckStyleServiceProvider.php
@@ -13,9 +13,12 @@ class ArtisanCheckstyleServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->publishes([
-            __DIR__.'/../../configs/checkstyle.php' => config_path('checkstyle.php'),
-        ]);
+        $this->publishes(
+            [
+                __DIR__.'/../../configs/checkstyle.php' => config_path('checkstyle.php'),
+            ],
+            'checkstyle'
+        );
     }
 
     /**
